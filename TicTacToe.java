a2d025c1e7f91b6990b10d476acdadb7043d6619
int-Butterz <khaledabouhechmeh@outlook.com>
2024-12-24 13:18:16 -0500

    created the board array and made the method that prints the board

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,8 +1,31 @@
-import java.util.Scanner;
 import java.util.Arrays;
 
 public class TicTacToe {
-    public static void main(String[] args) {
+    //board content
+    public static String[][] board = {
+            {" ", " ", " "},
+            {" ", " ", " "},
+            {" ", " ", " "}
+    };
 
+    //board output
+    public static String rowSeperator = "-";
+    public static String colSeperator = "\n|%1s|%1s|%1s|\n";
+
+    public static void main(String[] args) {
+        assembleBoard();
+    }
+
+    public static void assembleBoard() {
+        StringBuilder line = new StringBuilder();
+        for (int j = 0; j < colSeperator.length()/2; j++) {
+            line.append(rowSeperator);
+        }
+
+        System.out.print(line);
+        for (int i = 0; i < 3; i++) {
+            System.out.printf(colSeperator, board[i][0], board[i][1], board[i][2]);
+            System.out.print(line);
+        }
     }
 }
