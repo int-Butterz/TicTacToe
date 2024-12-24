5180459e9350dc8d988b849caaf8ec6d331bc526
int-Butterz <khaledabouhechmeh@outlook.com>
2024-12-24 12:48:41 -0500

    Added class

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class TicTacToe {
     public static void main(String[] args) {
-        System.out.println("Welcome to Tic-Tac-Toe!");
+
     }
 }
